f010206a6f281939d6e38dcff8065c87c678ff8c
Paolo D <p.donoriodemeo@elemize.com>
2021-03-14 22:57:29 +0100

    finding a path in matrix with dfs algo

---
+++ 1003_path.py
@@ -0,0 +1,46 @@
+def dfs(matrix: list, end: tuple, path: list, visited: list):
+    current = path[-1]
+    if current not in visited:
+        # print("current", current)
+        if current == end:
+            print(f"Path: {path}")
+            return True
+        visited.append(current)
+        x, y = current
+
+        checks = {
+            (x, y + 1): y < len(matrix) - 1,
+            (x, y - 1): y > 0,
+            (x + 1, y): x < len(matrix) - 1,
+            (x - 1, y): x > 0,
+        }
+
+        for nb, condition in checks.items():
+            # print(nb, condition)
+            # if condition:
+            #     print(matrix[nb[0]][nb[1]])
+            if condition and matrix[nb[0]][nb[1]]:
+                if dfs(matrix, end, path + [nb], visited):
+                    return True
+    return False
+
+
+if __name__ == '__main__':
+
+    matrix = [
+        [1, 1, 0, 1, 0],
+        [0, 1, 0, 1, 1],
+        [1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 0],
+        [1, 1, 0, 1, 1],
+    ]
+
+    tests = [
+        ((0, 0), (3, 3)),
+        ((0, 1), (0, 4)),
+        ((1, 4), (2, 0)),
+    ]
+
+    for points in tests:
+        print("Points", points)
+        print(dfs(matrix, end=points[1], path=[points[0]], visited=[]))
