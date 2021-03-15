40d745ecefd17ff9fd8504e15bd287675ce0b37c
Paolo D <p.donoriodemeo@elemize.com>
2021-03-16 00:10:04 +0100

    testing a real path solution on codewars. learning that a stack and a while loop is a possible alternative to recursion!

---
+++ 22_exit.py
@@ -0,0 +1,56 @@
+"""
+A solution to consider, based on a queue of steps to check:
+https://www.codewars.com/kata/reviews/5ad99956ff54ef8813000cf8/groups/5c704ff140972e00016cc25b
+
+>>> path_finder("\\n".join([]))
+False
+>>> path_finder("\\n".join([ ".W.", ".W.", "..." ]))
+True
+>>> path_finder("\\n".join([ ".W.", ".W.", "W.." ]))
+False
+>>> path_finder("\\n".join([ "......", "......", "......", "......", "......", "......" ]))
+True
+>>> path_finder("\\n".join([ "......", "......", "......", "......", ".....W", "....W." ]))
+False
+"""
+
+
+def walk(matrix, path, visited, dim, exit, wall='W'):
+    """
+    Recursive function to walk a matrix path,
+    avoiding to get out of index or to hit a wall ('W' symbol)
+
+    NOTE: matrix is a squared matrix (dim x dim)
+    """
+    position = path[-1]
+    if position == exit:
+        return True
+
+    visited.append(position)
+    x, y = position
+    strategies = {
+        "bottom": (x + 1 < dim, (x + 1, y)),
+        "top": (x - 1 > 0, (x - 1, y)),
+        "left": (y - 1 > 0, (x, y - 1)),
+        "right": (y + 1 < dim, (x, y + 1)),
+    }
+
+    for label, (check_boundaries, (j, k)) in strategies.items():
+        # if x >= 0 and y >= 0 and x < dim and y < dim:
+        if check_boundaries and (j, k) not in visited and matrix[j][k] != wall:
+            if walk(matrix, path + [(j, k)], visited, dim, exit):
+                return True
+    return False
+
+
+def path_finder(maze, start=(0, 0)):
+    matrix = [row for row in maze.splitlines()]
+    dim = len(matrix)
+    exit = (dim - 1, dim - 1)
+    return walk(matrix, [start], list(), dim, exit)
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
